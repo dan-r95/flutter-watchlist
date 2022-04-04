eb50b62ccbb3214bf3861d91390264aa2a36fdd2
dan-r95 <d.rossburg@googlemail.com>
2022-04-04 10:41:03 +0200

    MISC: mc

--- .github/workflows/writeDate.sh
+++ .github/workflows/writeDate.sh
@@ -1,4 +1,4 @@
 date=`date`
 commit=`git rev-parse --short HEAD`
 branch=`git rev-parse --abbrev-ref HEAD`
-echo "const buildTime = '${date}'; const buildCommit = '${commit} / ${branch}'; GOOGLE_CLIENT_ID = 'qDZW8-xH9-b013ARfMO10ybS'" > lib/login/build_info.dart
+echo "const buildTime = '${date}'; const buildCommit = '${commit} / ${branch}'; const GOOGLE_CLIENT_ID = 'qDZW8-xH9-b013ARfMO10ybS';" > lib/login/build_info.dart
